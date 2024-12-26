00495a8470ef5d17a23ead6aa1763e3c8f2017cb
bixboy <pierrelamare62@gmail.com>
2024-12-26 19:34:03 +0100

    Setup Projectil

---
+++ Source/RtsGame/Private/Components/WeaponMaster.cpp
@@ -0,0 +1,82 @@
+﻿#include "Components/WeaponMaster.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+FVector UWeaponMaster::GetDirection()
+{
+	APlayerCameraManager* Camera = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
+
+	const FVector Start = Camera->GetCameraLocation();
+	const FVector End = (UKismetMathLibrary::GetForwardVector(Camera->GetCameraRotation()) * WeaponRange) + Start;
+	
+	return PerformSingleLineTrace(Start, End, ECollisionChannel::ECC_Visibility).TraceEnd;
+}
+
+TArray<FVector> UWeaponMaster::GetShootTrace(FVector Direction)
+{
+	FVector TraceDirection = Direction;
+	const FVector Start = GetSocketLocation("Ammo");
+
+	if (HasAiming)
+	{
+		const FVector End = (((TraceDirection - Start) / (TraceDirection - Start).Length()) * 15000.f + TraceDirection)
+		+ FVector(
+			UKismetMathLibrary::RandomFloatInRange(-70.f, 70.f), // Vector X
+			UKismetMathLibrary::RandomFloatInRange(-70.f, 70.f), // Vector Y
+			UKismetMathLibrary::RandomFloatInRange(-70.f, 70.f) // Vector Z
+			);
+		
+		FHitResult HitResult = PerformSingleLineTrace(Start, End, ECollisionChannel::ECC_Visibility);
+		if (HitResult.GetActor())
+		{
+			return{HitResult.TraceStart, HitResult.ImpactPoint};
+		}
+		else
+		{
+			return{HitResult.TraceStart, HitResult.TraceEnd};
+		}
+	}
+	else
+	{
+		const FVector End = (((TraceDirection - Start) / (TraceDirection - Start).Length()) * 15000.f + TraceDirection)
+		+ FVector(
+			UKismetMathLibrary::RandomFloatInRange(-750.f, 750.f), // Vector X
+			UKismetMathLibrary::RandomFloatInRange(-750.f, 750.f), // Vector Y
+			UKismetMathLibrary::RandomFloatInRange(-750.f, 750.f) // Vector Z
+			);
+		
+		FHitResult HitResult = PerformSingleLineTrace(Start, End, ECollisionChannel::ECC_Visibility);
+		if (HitResult.GetActor())
+		{
+			return{HitResult.TraceStart, HitResult.ImpactPoint};
+		}
+		else
+		{
+			return{HitResult.TraceStart, HitResult.TraceEnd};
+		}
+	}
+}
+
+FHitResult UWeaponMaster::PerformSingleLineTrace(const FVector& Start, const FVector& End, ECollisionChannel TraceChannel) const
+{
+	FHitResult HitResult;
+	
+	FCollisionQueryParams TraceParams;
+	TraceParams.bTraceComplex = true;
+	TraceParams.bReturnPhysicalMaterial = false;
+	TraceParams.AddIgnoredActor(GetOwner());
+
+	GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, TraceChannel, TraceParams);
+	return HitResult;
+}
+
+void UWeaponMaster::SpawnBullet()
+{
+	FVector SpawnLocation = GetSocketLocation("Ammo");
+	FRotator SpawnRotation = UKismetMathLibrary::FindLookAtRotation(GetShootTrace(GetDirection())[0], GetShootTrace(GetDirection())[1]);
+	
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	GetWorld()->SpawnActor<AActor>(BulletClass, SpawnLocation, SpawnRotation, SpawnParams);
+}
---
+++ Source/RtsGame/Public/Components/WeaponMaster.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "WeaponMaster.generated.h"
+
+UCLASS()
+class RTSGAME_API UWeaponMaster : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+
+protected:
+	/*- Function -*/
+	UFUNCTION(BlueprintCallable)
+	FVector GetDirection();
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetShootTrace(FVector Direction);
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnBullet();
+	
+	UFUNCTION()
+	FHitResult PerformSingleLineTrace(const FVector& Start, const FVector& End, ECollisionChannel TraceChannel) const;
+
+	/*- Variables -*/
+	UPROPERTY(EditAnywhere, Category = "Settings|Weapon Values")
+	TObjectPtr<USkeletalMesh> WeaponMesh;
+	UPROPERTY(EditAnywhere, Category = "Settings|Weapon Values")
+	TSubclassOf<AActor> BulletClass;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings|Weapon Values")
+	float WeaponRange = 1000.f;
+	UPROPERTY(EditAnywhere, Category = "Settings|Weapon Values")
+	bool HasAiming;
+	
+};
+
